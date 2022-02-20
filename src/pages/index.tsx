3c6b7b047603e38cc14815a0cfcc2fa1f407e144
Zouhir HANAFI <zouhir.hanafi@gmail.com>
2022-02-20 10:57:19 +0100

    empty template

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import { CardType } from "types/models";
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto">
+        Empty Next.js project with tailwindcss
+      </div>
+    </div>
+  );
+};
+
+export default Home;
